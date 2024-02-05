0f24d519e7a8a43bf7c5f400b5ec38bffd394e89
awais512 <raza.awais25@gmail.com>
2024-02-05 22:34:50 +0000

    Login page

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+  email: z
+    .string()
+    .describe("Email")
+    .email({ message: "Please enter a valid email" }),
+  password: z
+    .string()
+    .describe("Password")
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
